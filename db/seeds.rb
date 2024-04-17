857ae247e580eb198c3b6338f3565234f61c8ecd
Ines Louro <inespazlouro@gmail.com>
2024-04-17 19:48:18 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'nokogiri'
+require 'open-uri'
+
+url = 'https://www.psychologies.co.uk/test/are-you-an-introvert-or-an-extrovert/'
+html = URI.open(url)
+doc = Nokogiri::HTML(html)
+
+questions = doc.css('.c-test__question-text')
+answers = doc.css('.c-test__answer-label .c-test__answer-text')
+
+puts 'Deleting Questions'
+Question.destroy_all
+
+puts 'Creating Questions and Answers'
+questions.each_with_index do |question, index|
+  new_question = Question.create!(body: question.text.strip)
+
+  score = 1
+  answers[index * 4, 4].each do |answer|
+    new_question.answers.create!(body: answer.text.strip, score:)
+    score += 1
+  end
+end
+
+puts 'Done'
